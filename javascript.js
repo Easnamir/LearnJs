45b620dee82e3748139f2b4157acce4e1b9c1d76
Amir Ahmed <easn.amirali@gmail.com>
2020-07-12 07:45:25 -0700

    made changes

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ var col2 = document.getElementById('color2');
 var body = document.getElementById('body');
 var css = document.getElementById('css');
 
-function getColor(){
+conts getColor = () => {
 
 body.style.background = "linear-gradient(to right, "+col1.value+", "+col2.value+")";
 css.textContent = body.style.background+";";
